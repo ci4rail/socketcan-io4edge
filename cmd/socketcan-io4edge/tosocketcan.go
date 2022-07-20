266a3bbfc9bf7c73745509d921f8de2812bac9a6
KlausPopp <klauspopp@gmx.de>
2022-07-20 14:13:36 +0200

    add error and RTR frames

--- cmd/socketcan-io4edge/tosocketcan.go
+++ cmd/socketcan-io4edge/tosocketcan.go
@@ -8,35 +8,127 @@ import (
 	"github.com/ci4rail/socketcan-io4edge/pkg/socketcan"
 )
 
+type canFrameCombined struct {
+	isErrorFrame bool
+	normalFrame  *socketcan.CANFrame
+	errorFrame   *socketcan.CANErrorFrame
+}
+
 func toSocketCAN(s *socketcan.RawInterface) {
 	// create a queue to buffer the received CAN frames from io4edge device
-	frameQ := make(chan *socketcan.CANFrame, 128)
+	frameQ := make(chan *canFrameCombined, 128)
 
 	// Go routine to read from io4edge device
 	go func() {
 		for {
 			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
 
-			f := &socketcan.CANFrame{
-				ID:       uint32(rand.Intn(0x7ff)),
-				DLC:      8,
-				Data:     []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08},
-				Extended: false,
+			f := &canFrameCombined{
+				isErrorFrame: false,
+				normalFrame: &socketcan.CANFrame{
+					ID:       uint32(rand.Intn(0x7ff)),
+					DLC:      8,
+					Data:     []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08},
+					Extended: false,
+					RTR:      false,
+				},
 			}
-			fmt.Printf("Generating frame: %s\n", f.String())
+
+			fmt.Printf("Generating standard frame: %s\n", f.normalFrame.String())
 			frameQ <- f
+
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+
+			f = &canFrameCombined{
+				isErrorFrame: false,
+				normalFrame: &socketcan.CANFrame{
+					ID:       uint32(rand.Intn(0x1fffffff)),
+					DLC:      4,
+					Data:     []byte{0xAA, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08},
+					Extended: true,
+					RTR:      false,
+				},
+			}
+
+			fmt.Printf("Generating extended frame: %s\n", f.normalFrame.String())
+			frameQ <- f
+
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+
+			f = &canFrameCombined{
+				isErrorFrame: false,
+				normalFrame: &socketcan.CANFrame{
+					ID:       uint32(rand.Intn(0x1fffffff)),
+					DLC:      4,
+					Data:     []byte{},
+					Extended: true,
+					RTR:      true,
+				},
+			}
+
+			fmt.Printf("Generating extended RTR frame: %s\n", f.normalFrame.String())
+			frameQ <- f
+
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+
+			f = &canFrameCombined{
+				isErrorFrame: true,
+				errorFrame: &socketcan.CANErrorFrame{
+					ErrorClass: socketcan.CANErrBusOff,
+				},
+			}
+
+			fmt.Printf("Generating bus error frame: %v\n", f.errorFrame)
+			frameQ <- f
+
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+
+			f = &canFrameCombined{
+				isErrorFrame: true,
+				errorFrame: &socketcan.CANErrorFrame{
+					ErrorClass:          socketcan.CANErrCtrl,
+					CANCtrlErrorDetails: socketcan.CANErrCtrlRxPassive,
+				},
+			}
+
+			fmt.Printf("Generating rx passive error frame: %v\n", f.errorFrame)
+			frameQ <- f
+
 		}
 	}()
 
 	// Go routine to write to socketcan
 	go func() {
 		for {
-			rxFrames := readFrameQ(frameQ)
+			rxFrames := readCombinedFrameQ(frameQ)
 			//fmt.Printf("Sending to socketcan\n")
 			for _, f := range rxFrames {
-				fmt.Printf(" send to sc %s\n", f.String())
-				s.Send(f)
+
+				if f.isErrorFrame {
+					//fmt.Printf(" send errorframe to sc %v\n", f.errorFrame)
+					s.SendErrorFrame(f.errorFrame)
+				} else {
+					//fmt.Printf(" send to sc %s\n", f.normalFrame.String())
+					s.Send(f.normalFrame)
+				}
 			}
 		}
 	}()
+}
+
+func readCombinedFrameQ(frameQ chan *canFrameCombined) []*canFrameCombined {
+	rxFrames := []*canFrameCombined{}
+	// wait for first frame
+	f := <-frameQ
+	rxFrames = append(rxFrames, f)
+
+	// read all other frames, but non-blocking
+	for {
+		select {
+		case f := <-frameQ:
+			rxFrames = append(rxFrames, f)
+		default: // queue is empty
+			return rxFrames
+		}
+	}
 }
--- pkg/socketcan/socketcan.go
+++ pkg/socketcan/socketcan.go
@@ -4,6 +4,8 @@ import (
 	"encoding/binary"
 	"fmt"
 
+	"log"
+
 	"golang.org/x/sys/unix"
 )
 
@@ -13,49 +15,60 @@ type CANFrame struct {
 	DLC      uint8
 	Data     []byte
 	Extended bool
+	RTR      bool
 }
 
-// ErrorClass represents athe CAN error class.
-type ErrorClass uint32
+// CANErrorClass represents athe CAN error class.
+type CANErrorClass uint32
 
-// ErrorFrameData0 represents details of arbitration lost error.
-type ErrorFrameData0 uint8
+// CANCtrlErrorDetails represents of a can controller error (when CANErrCtrl is set in CANErrorClass).
+type CANCtrlErrorDetails uint8
 
 const (
-	// CANErrTxTimeout flags a TX timeout (by netdevice driver)
-	CANErrTxTimeout ErrorClass = 0x00000001
-	// CANErrLostArb flags a lost arbitration, details in ErrorFrameData0
-	CANErrLostArb ErrorClass = 0x00000002
+	// CANErrTxTimeout flags a TX timeout
+	CANErrTxTimeout CANErrorClass = 0x00000001
+	// CANErrLostArb flags a lost arbitration
+	CANErrLostArb CANErrorClass = 0x00000002
 	// CANErrCtrl flags controller problems
-	CANErrCtrl ErrorClass = 0x00000004
+	CANErrCtrl CANErrorClass = 0x00000004
 	// CANErrProt flags protocol violations
-	CANErrProt ErrorClass = 0x00000008
+	CANErrProt CANErrorClass = 0x00000008
 	// CANErrTrx flags transceiver status
-	CANErrTrx ErrorClass = 0x00000010
+	CANErrTrx CANErrorClass = 0x00000010
 	// CANErrAck flags received no ACK on transmission
-	CANErrAck ErrorClass = 0x00000020
+	CANErrAck CANErrorClass = 0x00000020
 	// CANErrBusOff flags bus off
-	CANErrBusOff ErrorClass = 0x00000040
+	CANErrBusOff CANErrorClass = 0x00000040
 	// CANErrBusError flags bus error
-	CANErrBusError ErrorClass = 0x00000080
+	CANErrBusError CANErrorClass = 0x00000080
 	// CANErrRestarted flags controller restarted
-	CANErrRestarted ErrorClass = 0x00000100
+	CANErrRestarted CANErrorClass = 0x00000100
 
 	// CANErrCtrlUnspec flags unspecified
-	CANErrCtrlUnspec ErrorFrameData0 = 0x00
+	CANErrCtrlUnspec CANCtrlErrorDetails = 0x00
 	// CANErrCtrlRxOverflow flags RX buffer overflow
-	CANErrCtrlRxOverflow ErrorFrameData0 = 0x01
+	CANErrCtrlRxOverflow CANCtrlErrorDetails = 0x01
 	// CANErrCtrlTxOverflow flags TX buffer overflow
-	CANErrCtrlTxOverflow ErrorFrameData0 = 0x02
+	CANErrCtrlTxOverflow CANCtrlErrorDetails = 0x02
 	// CANErrCtrlRxWarning flags reached warning level for RX errors
-	CANErrCtrlRxWarning ErrorFrameData0 = 0x04
+	CANErrCtrlRxWarning CANCtrlErrorDetails = 0x04
 	// CANErrCtrlTxWarning flags reached warning level for TX errors
-	CANErrCtrlTxWarning ErrorFrameData0 = 0x08
+	CANErrCtrlTxWarning CANCtrlErrorDetails = 0x08
 	// CANErrCtrlRxPassive flags reached error passive status RX
-	CANErrCtrlRxPassive ErrorFrameData0 = 0x10
+	CANErrCtrlRxPassive CANCtrlErrorDetails = 0x10
 	// CANErrCtrlTxPassive flags reached error passive status TX
-	CANErrCtrlTxPassive ErrorFrameData0 = 0x20
+	CANErrCtrlTxPassive CANCtrlErrorDetails = 0x20
+
+	canErrFlag = 0x20000000
+	canRTRFlag = 0x40000000
+	canEFFFlag = 0x80000000
 )
+
+// CANErrorFrame represents a CAN error frame.
+type CANErrorFrame struct {
+	ErrorClass          CANErrorClass
+	CANCtrlErrorDetails CANCtrlErrorDetails
+}
 
 func (f *CANFrame) String() string {
 	var s string
@@ -64,6 +77,9 @@ func (f *CANFrame) String() string {
 		s += fmt.Sprintf("extended Frame %08x", f.ID)
 	} else {
 		s += fmt.Sprintf("standard Frame %03x", f.ID)
+	}
+	if f.RTR {
+		s += " (RTR)"
 	}
 
 	s += fmt.Sprintf(" DLC: %d, Data: ", f.DLC)
@@ -108,16 +124,28 @@ func (i *RawInterface) Close() error {
 }
 
 // Send sends a CAN frame.
+// blocking write
+// Use this function for standard and extended frames.
 func (i *RawInterface) Send(f *CANFrame) error {
 	frameBytes := make([]byte, 16)
-	// bytes 0-3: arbitration ID
-	if f.ID < 0x800 {
+	id := f.ID
+	if f.RTR {
+		id |= canRTRFlag
+	}
+
+	if !f.Extended {
 		// standard ID
-		binary.LittleEndian.PutUint32(frameBytes[0:4], f.ID)
+		if f.ID > 0x7FF {
+			return fmt.Errorf("ID %x is not a standard ID", f.ID)
+		}
+		binary.LittleEndian.PutUint32(frameBytes[0:4], id)
 	} else {
 		// extended ID
-		// set bit 31 (frame format flag (0 = standard 11 bit, 1 = extended 29 bit)
-		binary.LittleEndian.PutUint32(frameBytes[0:4], f.ID|1<<31)
+		if f.ID > 0x1FFFFFFF {
+			return fmt.Errorf("ID %x is not an extended ID", f.ID)
+		}
+		id |= canEFFFlag
+		binary.LittleEndian.PutUint32(frameBytes[0:4], id)
 	}
 
 	// byte 4: data length code
@@ -126,27 +154,68 @@ func (i *RawInterface) Send(f *CANFrame) error {
 	copy(frameBytes[8:], f.Data)
 
 	_, err := unix.Write(i.socket, frameBytes)
+	if err != nil {
+		log.Printf("Error writing to CAN socket: %v", err)
+	}
+	return err
+}
+
+// SendErrorFrame sends a CAN error frame.
+func (i *RawInterface) SendErrorFrame(f *CANErrorFrame) error {
+	frameBytes := make([]byte, 16)
+
+	id := uint32(f.ErrorClass | canErrFlag)
+	// bytes 0-3: ID
+	binary.LittleEndian.PutUint32(frameBytes[0:4], id)
+
+	// byte 4: data length code
+	frameBytes[4] = 8
+	// byte 9: Controller err details
+	frameBytes[9] = byte(f.CANCtrlErrorDetails)
+
+	_, err := unix.Write(i.socket, frameBytes)
+	if err != nil {
+		log.Printf("Error writing to CAN socket: %v", err)
+	}
 	return err
 }
 
 // Receive receives a CAN frame.
+// Blocking read
+// Handles only standard and extended frames, error frames are ignored
 func (i *RawInterface) Receive() (*CANFrame, error) {
-	f := CANFrame{}
-	frameBytes := make([]byte, 16)
-	_, err := unix.Read(i.socket, frameBytes)
-	if err != nil {
-		return nil, err
+	for {
+		f := CANFrame{}
+		frameBytes := make([]byte, 16)
+		_, err := unix.Read(i.socket, frameBytes)
+		if err != nil {
+			return nil, err
+		}
+
+		// bytes 0-3: ID
+		id := uint32(binary.LittleEndian.Uint32(frameBytes[0:4]))
+
+		if id&canErrFlag == 0 { // ignore error frames
+
+			if id&canEFFFlag == 0 {
+				// standard ID
+				f.ID = id & 0x7FF
+			} else {
+				// extended ID
+				f.ID = id & 0x1FFFFFFF
+				f.Extended = true
+			}
+			if id&canRTRFlag != 0 {
+				f.RTR = true
+			}
+
+			// byte 4: data length code
+			f.DLC = frameBytes[4]
+			// data
+			f.Data = make([]byte, 8)
+			copy(f.Data, frameBytes[8:])
+
+			return &f, nil
+		}
 	}
-
-	// bytes 0-3: arbitration ID
-	f.ID = uint32(binary.LittleEndian.Uint32(frameBytes[0:4]))
-	// remove bit 31: extended ID flag
-	f.ID &= 0x7FFFFFFF
-	// byte 4: data length code
-	f.DLC = frameBytes[4]
-	// data
-	f.Data = make([]byte, 8)
-	copy(f.Data, frameBytes[8:])
-
-	return &f, nil
 }
